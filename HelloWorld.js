92c1a8012cdd688e1132098b03815c9401674285
shashankkatte <43801946+shashankkatte@users.noreply.github.com>
2018-10-18 17:29:53 -0400

    Added a line on how to use alert
    Added comments and line on how to use alert in Js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,5 @@
+// This line prints outr a simple text but visible only in console window
 console.log("Hello World");
+
+// Another way of printing out is using an alert statement, use this paringly however as it can get annoying
+alert("Hello World");
